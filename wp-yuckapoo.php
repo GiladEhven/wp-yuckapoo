2efaec9cfd261e38731588e9402078cdf44cf465
Gilad Ehven <gilad@ehven.com>
2018-05-22 17:10:07 -0400

    First localhost commit: Base file system (including untested [composer.json]); parallels 'wordpress-plugin-starter-type-four'.

---
+++ wp-yuckapoo.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * WordPress plugin that adds issue tracker system with admin and public-side tools
+ *
+ * @author                  Gilad Ehven
+ * @copyright               2018 Gilad Ehven (Ehventerprise LLC)
+ * @license                 GNU General Public License 2.0+
+ * @link                    https://github.com/GiladEhven/wp-yuckapoo
+ * @package                 WpYuckapoo
+ * @since                   0.1.0
+ *
+ * @wordpress-plugin
+ *
+ * Plugin Name:             WP Yuckapoo
+ *
+ * Author:                  Gilad Ehven
+ * Author URI:              http://gilad-ehven.com
+ * Contributors:            Mihal Ehven (Ehventerprise LLC)
+ * Description:             This plugin adds an issue tracker system with admin and public-side tools.
+ * Domain Path:             /languages
+ * License:                 GNU General Public License 2.0+
+ * License URI:             http://www.gnu.org/licenses/gpl-2.0.txt
+ * Network:                 False
+ * Plugin URI:              https://github.com/GiladEhven/wp-yuckapoo
+ * Privacy Policy:          https://gilad-ehven.com/privacy/
+ * Terms of Use:            https://gilad-ehven.com/terms/
+ * Text Domain:             wp-yuckapoo
+ * Version:                 0.1.0
+ *
+ */
+
+namespace WpYuckapoo;
+
+if ( ! defined( 'ABSPATH' ) ) exit( "Nothing to see here. Sorry. Try the home page." );
